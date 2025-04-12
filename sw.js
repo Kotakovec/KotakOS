ae75058be8f94247f360a1c785f96dfe2638c823
Kotakovec <kotakjakub@gmail.com>
2025-04-13 00:02:54 +0200

    1

--- sw.js
+++ sw.js
@@ -1,19 +1,5 @@
-const CACHE_NAME = 'kotakos-app-cache-v1';
-const URLS_TO_CACHE = [
-  '/',
-  '/index.html',
-  '/styles.css',
-  '/script.js'
-];
-
-self.addEventListener('install', event => {
-  event.waitUntil(
-    caches.open(CACHE_NAME)
-      .then(cache => cache.addAll(URLS_TO_CACHE))
-});
-
-self.addEventListener('fetch', event => {
-  event.respondWith(
-    caches.match(event.request)
-      .then(response => response || fetch(event.request))
+const CACHE_NAME = 'kotakos-v1';
+self.addEventListener('install', (e) => {
+  e.waitUntil(caches.open(CACHE_NAME)
+    .then(cache => cache.addAll(['index.html', 'manifest.json'])));
 });
