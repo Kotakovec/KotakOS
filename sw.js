dcf684a0dcdef0d1424e9b712362c7258d2f95dd
Kotakovec <kotakjakub@gmail.com>
2025-04-13 08:39:53 +0200

    1

--- sw.js
+++ sw.js
@@ -1,5 +1,22 @@
-const CACHE_NAME = 'kotakos-v1';
-self.addEventListener('install', (e) => {
-  e.waitUntil(caches.open(CACHE_NAME)
-    .then(cache => cache.addAll(['index.html', 'manifest.json'])));
+const CACHE_NAME = 'pokracovat-app-v1';
+const urlsToCache = [
+  '/',
+  '/index.html',
+  '/manifest.json'
+];
+
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(cache => cache.addAll(urlsToCache))
+});
+
+self.addEventListener('fetch', event => {
+  if (event.request.url.includes('nicepage.io')) {
+    event.respondWith(fetch(event.request));
+  } else {
+    event.respondWith(
+      caches.match(event.request)
+        .then(response => response || fetch(event.request))
+  }
 });
